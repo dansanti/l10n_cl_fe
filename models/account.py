2c490dc70aca24eefb14bb4bb6bbcd55335684f1
Daniel Santibáñez Polanco <dansanti@gmail.com>
2017-12-16 18:16:54 -0300

    Código obsoleto

--- models/account.py
+++ models/account.py
@@ -134,17 +134,6 @@ class SiiTax(models.Model):
 class account_move(models.Model):
     _inherit = "account.move"
 
-    def _get_document_data(self, cr, uid, ids, name, arg, context=None):
-        """ TODO """
-        res = {}
-        for record in self.browse(cr, uid, ids, context=context):
-            document_number = False
-            if record.model and record.res_id:
-                document_number = self.pool[record.model].browse(
-                    cr, uid, record.res_id, context=context).document_number
-            res[record.id] = document_number
-        return res
-
     @api.depends(
         'sii_document_number',
         'name',
