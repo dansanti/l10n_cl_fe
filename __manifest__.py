b23bed478fac47862c5987906eb92a304a6373f8
Daniel Santibáñez Polanco <dansanti@gmail.com>
2017-12-26 23:35:47 -0300

    [FIX]Resolución según manual muestras impresas SII

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": """Facturación Electrónica para Chile\
     """,
-    'version': '11.0.0.0.39',
+    'version': '11.0.0.0.40',
     'category': 'Localization/Chile',
     'sequence': 12,
     'author':  'Daniel Santibáñez Polanco, Cooperativa OdooCoop',
