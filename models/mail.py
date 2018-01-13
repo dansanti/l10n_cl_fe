a5fe13cb7c61475aae0530a0f50a8fab50ac684b
celm1990 <celm1990@hotmail.com>
2018-01-12 22:48:26 -0500

    FIX: declarar variable para evitar error de variable no declarada

--- models/mail.py
+++ models/mail.py
@@ -73,6 +73,7 @@ class ProccessMail(models.Model):
 
     @api.multi
     def process_message(self, pre=False, option=False):
+        created = False
         for r in self:
             for att in r.sudo().mail_id.attachment_ids:
                 if not att.name:
