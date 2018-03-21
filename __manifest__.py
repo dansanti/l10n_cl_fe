4194c9445e6ba5f3157292ab6c7c078b6980db6f
Daniel Santibáñez Polanco <dansanti@gmail.com>
2018-03-21 20:01:12 -0300

    [FIX]Contexto para GDR facuras de proveedor

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": """Facturación Electrónica para Chile\
     """,
-    'version': '0.1.2',
+    'version': '0.1.3',
     'category': 'Localization/Chile',
     'sequence': 12,
     'author':  'Daniel Santibáñez Polanco, Cooperativa OdooCoop',
